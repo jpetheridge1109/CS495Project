1f20526a6a58decb7df33cf4165d3bbcdb219ace
Nathan Wiechec <nlwiechec@crimson.ua.edu>
2023-02-24 16:43:37 -0600

    skeleton for event popup

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Modal, Alert } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <Text>Jajajaja!</Text>
+      <Modal
+        animationType = "fade"
+        transparent = {true}
+        visible = {modalVisible}
+        onRequestClose = {() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible)
+        }}>
+        <View style = {styles.centered}>
+          <View style = {styles.modalView}>
+          <Text style = {styles.modalText}>Event Name</Text>
+            <View style = {{flexDirection: 'row', marginBottom: 20}}>
+              <Text style = {styles.modalLeftText}>Date of Event</Text>
+              <Text style = {styles.modalRightText}>Place of Event</Text>
+
+            </View>
+            <View style = {{flexDirection: 'row', marginBotom: 20}}>
+              <Text style = {styles.modalLeftText}>Time of Event</Text>
+              <Text style = {styles.modalRightText}>Event Organizer</Text>
+            </View>
+            <View style = {{flexDirection: 'row', marginBottom: 20}}>
+              <Pressable style = {[styles.button, styles.buttonRSVP]}>
+                <Text style = {styles.textStyle}>RSVP</Text>
+              </Pressable>
+              <Pressable style = {[styles.button, styles.buttonViewPpl]}>
+                <Text style = {styles.textStyle}>View People Attending</Text>
+              </Pressable>
+            </View>
+            <Pressable
+              style = {[styles.button, styles.buttonClose]}
+              onPress = {() => setModalVisible(!modalVisible)}>
+              <Text style = {styles.textStyle}>Hide This Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style = {[styles.button, styles.buttonOpen]}
+        onPress = {() => setModalVisible(true)}>
+        <Text style = {styles.textStyle}>This would be an event to click on</Text>
+      </Pressable>  
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +59,72 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    verticalAlign: 'center',
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    //alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  buttonRSVP: {
+    backgroundColor: '#DA8E12',
+    justifyContent: 'flex-start',
+    flex: 1,
+    marginHorizontal: 10,
+  },
+  buttonViewPpl: {
+    backgroundColor: '#DA8E12',
+    justifyContent: 'flex-end',
+    flex: 1,
+    marginHorizontal: 10,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  modalLeftText: {
+    flex: 1,
+    marginBottom: 15,
+    textAlign: 'left',
+    justifyContent: 'flex-start',
+  },
+  modalRightText: {
+    flex: 1,
+    marginBottom: 15,
+    textAlign: 'right',
+    justifyContent: 'flex-end',
   },
 });
