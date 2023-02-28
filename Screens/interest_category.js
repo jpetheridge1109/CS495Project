1cb288e26596de779c8219ef8dc47b62fa944ddb
kpsokol1 <sokolky20@stxtigers.com>
2023-02-27 19:02:50 -0600

    Added navigation between screens, and added screen with specific groups per category when you click on the category

---
+++ Screens/interest_category.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  Image
+} from 'react-native';
+
+import {Stack, Surface} from "@react-native-material/core";
+import {SearchBar} from "react-native-elements";
+import { Avatar, Divider } from '@rneui/themed';
+
+const DATA = [
+  {
+    id: '1',
+    color: '#9285ef',
+    name: 'Mountain Biking',
+    img: require('../assets/bike.png'),
+    numMembers: 20,
+    description: 'We organize group rides for mountain and gravel biking in Tuscaloosa. We typically meet at Sokol Park on the weekends. All skill levels are welcome!'
+  },
+  {
+    id: '2',
+    color: '#76a9f8',
+    name: 'French Students at UA',
+    img: require('../assets/Flag_of_France.png'),
+    numMembers: 32,
+    description: 'We are French Students currently studying abroad at UA. We know life abroad can be stressful, so our goal is to help other French students find new friends on the other side of the pond. Come join us to eat, play, and have fun!'
+  },
+  {
+    id: '3',
+    color: '#f8d4b2',
+    name: 'Monopoly',
+    img: require('../assets/monopoly.png'),
+    numMembers: 15,
+    description: 'Like playing Monopoly? If so, this group is for you. We organize game nights and tournaments every week!'
+  }
+];
+
+const Item = ({item}) => (
+    <TouchableOpacity>
+      <Surface
+        elevation={20}
+        category="medium"
+        style={{ alignSelf: 'center', width: '90%', aspectRatio: 5, marginBottom: 20, borderRadius: 10}}
+      >
+        <View style={{flex: 1, flexDirection: "row"}}>
+          <View style={{flex: 1}}>
+            <Image
+                source= {item.img} style={styles.avatars}/>
+                <Text style={styles.memberCount}>{item.numMembers} members</Text>
+          </View>
+          <Divider orientation = 'vertical' width = '2'/>
+          <View style={{flex: 3}}>
+            <Text style={styles.groupNameText}>{item.name}</Text>
+            <Text style = {styles.description}>{item.description}</Text>
+          </View>
+
+
+        </View>
+      </Surface>
+    </TouchableOpacity>
+
+);
+
+
+export default class Interest_category extends React.Component{
+  render (){
+    return(
+         <SafeAreaView style={styles.container}>
+           <Text style={styles.categories}>Groups</Text>
+           <FlatList
+               data={DATA}
+               renderItem={({item}) => <Item item={item}/>}
+               keyExtractor={item => item.id}
+           />
+         </SafeAreaView>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight || 0,
+    backgroundColor: '#344e71'
+  },
+  item: {
+    width: '35%',
+    aspectRatio: 1,
+    marginVertical: 16,
+    marginHorizontal: 16,
+    borderRadius:30,
+    justifyContent: 'center'
+  },
+  categories:{
+    paddingTop: 20,
+    textAlign: 'center',
+    fontSize: 40,
+    color: '#e9dfdf',
+    paddingBottom:20
+  },
+  text:{
+    paddingTop: '2%',
+    pattingBottom: '2%',
+    textAlign: 'center',
+    fontSize: '40%',
+    color: '#343333',
+  },
+  image:{
+    width: '50%',
+    height: undefined,
+    aspectRatio: 1,
+    alignSelf: 'center'
+  },
+  avatars:{
+    alignSelf: 'center',
+    justifySelf: 'center',
+    height: '75%',
+    aspectRatio:1,
+    borderRadius: 10000,
+    marginTop: '5%'
+  },
+  groupNameText:{
+    alignSelf: 'center',
+    fontSize: '40%',
+
+  },
+  memberCount: {
+    alignSelf:'center',
+    paddingTop: '2%',
+    fontSize:'20%',
+    color: '#999191',
+  },
+  description:{
+    paddingLeft:'2%',
+    fontSize: '20%',
+    color: '#999191',
+  }
+});
---
+++ Screens/interest_search.js
@@ -0,0 +1,194 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  Image,
+    Button
+} from 'react-native';
+import {SearchBar} from 'react-native-elements';
+
+const DATA = [
+  {
+    id: '1',
+    color: '#9285ef',
+    name: 'Sports',
+    img: require('../assets/balls-sports.png')
+  },
+  {
+    id: '2',
+    color: '#76a9f8',
+    name: 'Video Games',
+    img: require('../assets/game-console.png')
+  },
+  {
+    id: '3',
+    color: '#f8d4b2',
+    name: 'Food',
+    img: require('../assets/diet.png')
+  },
+  {
+    id: '4',
+    color: '#6fd6d3',
+    name: 'Music',
+    img: require('../assets/musical-note.png')
+  },
+  {
+    id: '5',
+    color: '#fb8ba4',
+    name: 'Pets',
+    img: require('../assets/pets.png')
+  },
+  {
+    id: '6',
+    color: '#da8cf1',
+    name: 'Shopping',
+    img: require('../assets/shopping-cart.png')
+  },
+  {
+    id: '7',
+    color: '#2ed38f',
+    name: 'Workout',
+    img: require('../assets/dumbbell.png')
+  },
+  {
+    id: '8',
+    color: '#e58b8a',
+    name: 'Academic',
+    img: require('../assets/education.png')
+  },
+  {
+    id: '9',
+    color: '#a5def9',
+    name: 'Reading',
+    img: require('../assets/read.png')
+  },
+  {
+    id: '10',
+    color: '#c1b4be',
+    name: 'Volunteering',
+    img: require('../assets/help.png')
+  },
+  {
+    id: '11',
+    color: '#baffc9',
+    name: 'Outdoors',
+    img: require('../assets/outdoors.png')
+  },
+  {
+    id: '12',
+    color: '#FAFFC7',
+    name: 'LQBTQ+',
+    img: require('../assets/progress.png')
+  },
+  {
+    id: '13',
+    color: '#FD8A8A',
+    name: 'International',
+    img: require('../assets/earth.png')
+  },
+  {
+    id: '14',
+    color: '#A8D1D1',
+    name: 'Movies',
+    img: require('../assets/popcorn.png')
+  },
+  {
+    id: '15',
+    color: '#ffdfba',
+    name: 'Photography',
+    img: require('../assets/camera.png')
+  },
+  {
+    id: '15',
+    color: '#FDFDBD',
+    name: 'Board Games',
+    img: require('../assets/board-game.png')
+  },
+];
+
+
+
+const updateSearch = (search) => {
+  this.setState({ search });
+};
+
+export default class Interest_search extends React.Component{
+  state = {
+    search: '',
+  };
+
+  updateSearch = (search) => {
+    this.setState({ search });
+  };
+  render (){
+    const { search } = this.state;
+    const Item = ({item}) => (
+        <TouchableOpacity style={[styles.item,{backgroundColor: item.color}]} onPress={() => this.props.navigation.navigate('Profile')}>
+          <Text style={styles.text}>{item.name}</Text>
+          <Image
+              source= {item.img} style = {styles.image}/>
+        </TouchableOpacity>
+    );
+    return(
+        <SafeAreaView style={styles.container}>
+          <Text style={styles.categories} >Categories</Text>
+          <SearchBar
+              containerStyle={{backgroundColor: 'white', borderRadius: 30, marginHorizontal:'5%'}}
+              placeholder="Type Here..."
+              onChangeText={this.updateSearch}
+              value={search}
+              lightTheme = 'true'
+              style = {styles.search}
+          />
+          <FlatList
+              columnWrapperStyle={{justifyContent: 'space-evenly'}}
+              data={DATA}
+              numColumns={2}
+              renderItem={({item}) => <Item item={item}/>}
+              keyExtractor={item => item.id}
+          />
+        </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight || 0,
+    backgroundColor: '#344e71'
+  },
+  item: {
+    width: '35%',
+    aspectRatio: 1,
+    marginVertical: 16,
+    marginHorizontal: 16,
+    borderRadius:30,
+    justifyContent: 'center'
+  },
+  categories:{
+    paddingTop: 20,
+    textAlign: 'center',
+    fontSize: 40,
+    color: '#e9dfdf',
+    paddingBottom:20
+  },
+  text:{
+    paddingTop: '2%',
+    pattingBottom: '2%',
+    textAlign: 'center',
+    fontSize: '40%',
+    color: '#343333',
+  },
+  image:{
+    width: '50%',
+    height: undefined,
+    aspectRatio: 1,
+    alignSelf: 'center'
+  }
+});
