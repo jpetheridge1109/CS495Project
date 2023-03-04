741afed88e1c315542838b9d49ee4e525cfa9438
Kyle McCann <kemccann@crimson.ua.edu>
2023-03-04 23:04:32 +0000

    Fixed merged conflicts

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin', // This line added
+      'react-native-reanimated/plugin',
     ],
   };
 };
