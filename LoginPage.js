11c4535385c7aca82d6e0f7a649637c8593e19cd
jpetheridge <jpetheridge@crimson.ua.edu>
2023-03-28 13:00:19 -0500

    cleaned up LoginPage

--- LoginPage.js
+++ LoginPage.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
@@ -7,115 +6,6 @@ import ProfSetName from './screens/ProfSet_Name.js'
 import ProfSetDetails from './screens/ProfSet_Details.js'
 
 const Stack = createStackNavigator();
-/*
-function ProfileSetupStack() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Name" component={ProfSetName} />
-      <Stack.Screen name="Details" component={ProfSetDetails} />
-    </Stack.Navigator>
-  );
-}
-
-
-export default function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigation = useNavigation();
-
-  const handleLogin = () => {
-    // Here you can perform your login logic, such as sending a request to your backend API
-    // using axios or another HTTP client library.
-
-    // After the login is successful, navigate to the next screen.
-    navigation.navigate('Find a Group');
-  };
-
-  const handleSignIn = () => {
-    navigation.navigate('Name');
-  };
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>Login</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        onChangeText={setEmail}
-        value={email}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        onChangeText={setPassword}
-        value={password}
-      />
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
-        <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
-      <View style={styles.signupTextContainer}>
-        <Text style={styles.signupText}>Don't have an account? </Text>
-        <TouchableOpacity onPress={handleSignIn}>
-          <Text style={styles.signupLink}>Sign up here</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-    marginTop: -160,
-  },
-  heading: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 30,
-  },
-  input: {
-    width: '80%',
-    height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    borderRadius: 4,
-    paddingHorizontal: 10,
-    marginBottom: 20,
-  },
-  button: {
-    backgroundColor: 'blue',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 4,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  signupTextContainer: {
-    flexDirection: 'row',
-    paddingVertical: 30,
-  },
-  signupText: {
-    fontSize: 16,
-  },
-  signupLink: {
-    fontSize: 16,
-    color: 'blue',
-    textDecorationLine: 'underline',
-  },
-});
-
-*/
-
-
-
-
 
 export default function LoginPage() {
   return (
@@ -133,10 +23,7 @@ function LoginHome() {
   const navigation = useNavigation();
 
   const handleLogin = () => {
-    // Here you can perform your login logic, such as sending a request to your backend API
-    // using axios or another HTTP client library.
-
-    // After the login is successful, navigate to the next screen.
+    // After the login is successful, navigate to the home screen.
     navigation.navigate('Find a Group');
   };
 
