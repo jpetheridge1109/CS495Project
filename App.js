62475506aea59c61f6278fef3f3eee9028930524
kpsokol1 <sokolky20@stxtigers.com>
2023-03-27 21:58:20 -0500

    Initial chat working

--- App.js
+++ App.js
@@ -1,22 +1,83 @@
-import * as React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import 'react-native-gesture-handler';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import Event  from './modals/Event.js'
-import Profile from './screens/Profile.js'
-import GroupFind from './GroupFind.js'
-import SettingsHomepage from './screens/SettingsHomepage.js'
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text } from 'react-native';
+import { useChatClient } from './useChatClient';
+import {AppProvider, useAppContext} from "./AppContext";
+import App from "react-native/template/App";
+import {ChannelList, Chat, OverlayProvider, Channel,  MessageList,  MessageInput} from 'stream-chat-expo'; // Or stream-chat-expo
+import { StreamChat } from 'stream-chat';
+import { chatApiKey, chatUserId } from './chatConfig';
 
-const Drawer = createDrawerNavigator();
+const chatClient = StreamChat.getInstance(chatApiKey);
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const filters = {
+  members: {
+    '$in': [chatUserId]
+  },
+};
+
+const sort = {
+  last_message_at: -1,
+};
+
+
+const ChannelScreen = props => {
+  const { channel } = useAppContext();
+
   return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Find A Group">
-      <Drawer.Screen name="Find a Group" component={GroupFind} />
-        <Drawer.Screen name="Profile" component={Profile} />
-        <Drawer.Screen name="Event" component={Event} />
-        <Drawer.Screen name="Preferences" component={SettingsHomepage} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+      <Channel channel={channel}>
+        <MessageList />
+        <MessageInput />
+      </Channel>
   );
-}
+};
+
+const ChannelListScreen = props => {
+  const { setChannel } = useAppContext();
+  return (
+      <ChannelList
+          onSelect={(channel) => {
+            const { navigation } = props;
+            setChannel(channel);
+            navigation.navigate('ChannelScreen');
+          }}
+      />
+  );
+};
+
+
+
+const NavigationStack = () => {
+  const { clientIsReady } = useChatClient();
+
+  if (!clientIsReady) {
+    return <Text>Loading chat ...</Text>
+  }
+  return (
+      <OverlayProvider>
+        <Chat client={chatClient}>
+        <Stack.Navigator>
+          <Stack.Screen name="ChannelList" component={ChannelListScreen} />
+          <Stack.Screen name="ChannelScreen" component={ChannelScreen} />
+        </Stack.Navigator>
+        </Chat>
+      </OverlayProvider>
+  );
+};
+
+export default () => {
+  return (
+      <AppProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <NavigationStack />
+        </NavigationContainer>
+      </SafeAreaView>
+      </AppProvider>
+  );
+};
